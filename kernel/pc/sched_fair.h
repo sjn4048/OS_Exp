4fc90e97d8b1995e6180bda658bd23894b03e5fe
AlanShaw-GitHub <alanshawzju@gmail.com>
2018-12-28 20:40:39 +0800

    c

--- kernel/pc/sched_fair.h
+++ kernel/pc/sched_fair.h
@@ -42,6 +42,29 @@ static const int prio_to_wmult[40] = {
  /*  15 */ 119304647, 148102320, 186737708, 238609294, 286331153,
 };
 
+struct sched_class {
+
+	struct task_struct * (*pick_next_task) ();
+
+	// void (*enqueue_task) (struct rq *rq, struct task_struct *p, int flags);
+	// void (*dequeue_task) (struct rq *rq, struct task_struct *p, int flags);
+	// void (*yield_task) (struct rq *rq);
+
+	// void (*check_preempt_curr) (struct rq *rq, struct task_struct *p, int flags);
+
+	// void (*put_prev_task) (struct rq *rq, struct task_struct *p);
+
+	// void (*set_curr_task) (struct rq *rq);
+	// void (*task_tick) (struct rq *rq, struct task_struct *p, int queued);
+	// void (*task_fork) (struct task_struct *p);
+
+	// void (*switched_from) (struct rq *this_rq, struct task_struct *task);
+	// void (*switched_to) (struct rq *this_rq, struct task_struct *task);
+	// void (*prio_changed) (struct rq *this_rq, struct task_struct *task, int oldprio);
+
+	// unsigned int (*get_rr_interval) (struct rq *rq, struct task_struct *task);
+
+};
 
 /* CFS-related fields in a runqueue */
 struct cfs_rq {
