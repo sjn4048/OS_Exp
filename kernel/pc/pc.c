37eacb2cbe27e124f14f6279c99e632c35dfb2cb
AlanShaw-GitHub <alanshawzju@gmail.com>
2018-12-29 22:17:05 +0800

    Update pc.c

--- kernel/pc/pc.c
+++ kernel/pc/pc.c
@@ -96,14 +96,11 @@ void init_pc() {
     INIT_LIST_HEAD(&all_waiting);
     INIT_LIST_HEAD(&all_ready);
 
-kernel_printf("----all_task--%x(------\n",(unsigned int)&all_task);
 
     // ---------------------------------------------------
     // ---------- setting idle task ----------------------
     task_union *new = (task_union*)(kernel_sp - TASK_KERNEL_SIZE);
     task_struct * task = &(new->task);
-kernel_printf("--kernel_sp----%x(------\n",kernel_sp);
-kernel_printf("--&kernel_sp----%x(------\n",(unsigned int)&kernel_sp);
     // allocate a PID
     task->PID = cur_PID++;
     // idle task does not has parent
@@ -211,7 +208,6 @@ void pc_schedule(unsigned int status, unsigned int cause, context* pt_context) {
 void pc_create(char *task_name, void(*entry)(unsigned int argc, void *args), unsigned int argc, void *args, int nice) {
 
     task_union *new = (task_union*) kmalloc(sizeof(task_union));
-kernel_printf("--kmalloc----%x(-----\n",(unsigned int)new);
     task_struct * task = &(new->task);
     kernel_strcpy(task->name, task_name);
     INIT_LIST_HEAD(&(task->task_list));
@@ -329,6 +325,7 @@ int pc_exit(unsigned int status, unsigned int cause, context* pt_context){
     // switch context registers
     copy_context(&(current_task->context),pt_context);
 
+    enable_interrupts();
     return 0;
 }
 
