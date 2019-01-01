2acc9cc23d9cd64a668e88ed0ec24eb4a58bde81
AlanShaw-GitHub <alanshawzju@gmail.com>
2019-01-01 23:37:45 +0800

    Update init.c

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -2,11 +2,6 @@
 
 int main(unsigned int argc, void *args) {
     syscall(4);
-    // asm volatile(
-    //     "li $t0, 4\n\t"
-    //     "syscall\n\t"
-    //     "nop\n\t"
-    // );
     return (int)argc;
 }
 
