0d990d151d6d01fbaa7eee171642e71d03366de3
AlanShaw-GitHub <alanshawzju@gmail.com>
2019-01-01 22:29:19 +0800

    aa

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -1,11 +1,11 @@
 #include <init.h>
 
 int main(unsigned int argc, void *args) {
-    // syscall(4);
-    asm volatile(
-        "li $t0, 4\n\t"
-        "syscall\n\t"
-        "nop\n\t"
-        );
+    syscall(4);
+    // asm volatile(
+    //     "li $t0, 4\n\t"
+    //     "syscall\n\t"
+    //     "nop\n\t"
+    // );
     return (int)argc;
 }
