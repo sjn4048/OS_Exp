c20d303da6674558247ecf7297f0d44461f1ad6d
Alan-Shaw <alanshawzju@gmail.com>
2019-01-01 21:20:01 +0800

    d

--- kernel/pc/pc.c
+++ kernel/pc/pc.c
@@ -617,8 +617,8 @@ int exec_from_file(char* filename) {
     }
     int (*f)(unsigned int argc, void *args, unsigned int addr) = (int (*)(unsigned int argc, void *args, unsigned int addr))(ENTRY);
     char argv[1][10];
-    kernel_memset(argv[1],0,sizeof(argv));
-    kernel_memcpy(argv[1],"test",5);
+    kernel_memset(argv,0,sizeof(argv));
+    kernel_memcpy(argv[0],"test",sizeof("test"));
     int ret = f(1,(void *)argv,ENTRY);
     // pc_create("seg",(void *)ENTRY,0,0,0,1);
     // kfree((void*)ENTRY);
