cfa357f876946c68e260576e1f770fc8a749053f
Alan-Shaw <alanshawzju@gmail.com>
2019-01-02 15:34:56 +0800

    s

--- arch/mips32/start.s
+++ arch/mips32/start.s
@@ -188,18 +188,15 @@ GetPC:
 
 .org 0x0900
 transform:
-	# add $t0, $a0, $a1
-	# addi $sp, $sp, -8
-	# sw $a1, 0($sp)
-	# sw $ra, 4($sp)
-	# move $a0, $a2
-	# move $a1, $a3
-	# # jal $t0
-	# # nop
-	# lw $t0, 0($sp)
-	# lw $ra, 4($sp)
-	# addiu $sp, $sp, 8
-	# add $ra, $ra, $a1
+	add $t0, $a0, $a1
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	move $a0, $a2
+	move $a1, $a3
+	jal $t0
+	nop
+	lw $ra, 0($sp)
+	addiu $sp, $sp, 4
 	jr $ra
 	nop
 
