29f1f5ac63bf2337346b6f50b9dc58d1dfb63487
Alan-Shaw <alanshawzju@gmail.com>
2019-01-02 18:48:21 +0800

    s

--- uu/usrprog/sdk/src/OSsdk.c
+++ uu/usrprog/sdk/src/OSsdk.c
@@ -1,7 +1,7 @@
 #include <OSsdk.h>
 
 
-unsigned int ENTRY_ADDR = 0;
+unsigned int ENTRY_ADDR;
 unsigned int TRANSFORM_ADDR;
 
 
@@ -10,6 +10,7 @@ int (*kernel_printf)(const char* format, ...) = (int (*)(const char* format, ...
 
 void sdk_init(unsigned int argc, void *args, unsigned int entry_point){
     ENTRY_ADDR = entry_point;
+    int (*kernel_printf)(const char* format, ...) = (int (*)(const char* format, ...))(0x80002838);
     TRANSFORM_ADDR = 0x80000900;
     TRANSFORM((unsigned int)main,0,0);
 }
