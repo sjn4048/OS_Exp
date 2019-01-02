bae4343012ae4fb9ff7919fbe7932a851200d367
Alan-Shaw <alanshawzju@gmail.com>
2019-01-02 17:22:12 +0800

    s

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -5,10 +5,11 @@ int test_jump(int arg){
 }
 
 char tmp[] = "13";
+int tes = 2;
 unsigned int main(unsigned int argc, void *args) {
     test4(23);
     unsigned int ra;
     asm volatile("move %0,$ra\n\t" : "=r"(ra)); 
-    return tmp;
+    return tes;
 }
 
