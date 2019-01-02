5bf64b9e074e42b935b1ff2ca91a95585f213fa8
Alan-Shaw <alanshawzju@gmail.com>
2019-01-02 14:32:07 +0800

    s

--- uu/usrprog/sdk/src/OSsdk.c
+++ uu/usrprog/sdk/src/OSsdk.c
@@ -4,14 +4,15 @@
 unsigned int ENTRY_ADDR = 0;
 const unsigned int TRANSFORM_ADDR = 0x80000900;
 
+// kernel_printf = ((int (*)(const char* format, ...))(0x80002838));
 
 
 void sdk_init(unsigned int argc, void *args, unsigned int entry_point){
     ENTRY_ADDR = entry_point;
-    unsigned int ra;
-    asm volatile("la %0, ra\n\t" : "=r"(ra)); 
-    TRANSFORM(kernel_printf,"%x",ra);
-    TRANSFORM(kernel_printf,"%x",ra);
+    unsigned int rsa;
+    asm volatile("move %0,$ra\n\t" : "=r"(rsa)); 
+    TRANSFORM(((int (*)(const char* format, ...))(0x80002838)),"%x\n",rsa);
+    TRANSFORM(((int (*)(const char* format, ...))(0x80002838)),"%x\n",rsa);
     TRANSFORM((unsigned int)main,0,0);
     // asm volatile(
     //     "la $t0, main\n\t"
