40f95933a39abac35ccf6727dc61d9f9a4ac0965
Alan-Shaw <alanshawzju@gmail.com>
2019-01-02 17:44:08 +0800

    s

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -9,7 +9,7 @@ unsigned int tes = 2;
 unsigned int main(unsigned int argc, void *args) {
     test4(23);
     unsigned int init_gp;
-    asm volatile("la %0, _gp\n\t" : "=r"(init_gp)); 
+    asm volatile("move %0, $gp\n\t" : "=r"(init_gp)); 
     return init_gp;
 }
 
