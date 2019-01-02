472642a09edb13033ebc3c707feb36922aa2839c
Alan-Shaw <alanshawzju@gmail.com>
2019-01-02 15:25:32 +0800

    s

--- uu/usrprog/sdk/src/OSsdk.c
+++ uu/usrprog/sdk/src/OSsdk.c
@@ -13,7 +13,10 @@ void sdk_init(unsigned int argc, void *args, unsigned int entry_point){
     // asm volatile("move %0,$ra\n\t" : "=r"(rsa)); 
     // TRANSFORM(((int (*)(const char* format, ...))(0x80002838)),"%x\n",rsa);
     // TRANSFORM(((int (*)(const char* format, ...))(0x80002838)),"%x\n",rsa);
-    TRANSFORM((unsigned int)main,0,0);
+    // TRANSFORM((unsigned int)main,0,0);
+    ((unsigned int (*)(unsigned int func, \
+ unsigned int entry))                                          \
+(TRANSFORM_ADDR))((unsigned int)main,entry_point);
     // asm volatile(
     //     "la $t0, main\n\t"
     //     "add $t0, $t0, $a2\n\t"
