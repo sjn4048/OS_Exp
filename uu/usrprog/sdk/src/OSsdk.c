6337670f8273093475919f826be5d233c75e005a
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 03:17:20 +0800

    s

--- uu/usrprog/sdk/src/OSsdk.c
+++ uu/usrprog/sdk/src/OSsdk.c
@@ -9,7 +9,7 @@ unsigned int TRANSFORM_ADDR;
 void sdk_init(unsigned int argc, void *args, unsigned int entry_point){
     ENTRY_ADDR = entry_point;
     TRANSFORM_ADDR = 0x80000900;
-    printf = (int (*)(const char* format, ...))(0x8000283c);
+    printf = (int (*)(const char* format, ...))(0x8000282c);
     main(0,0);
     // TRANSFORM((unsigned int)main,0,0);
 }
