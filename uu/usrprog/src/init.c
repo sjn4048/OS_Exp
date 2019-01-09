d9fccd0a9f76c2185d32d7f13867f324b3f74c47
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 00:42:40 +0800

    d

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -9,9 +9,9 @@ unsigned int main(unsigned int argc, void *args) {
 
     char x[10] = "%dab\n";
     unsigned int ptr = malloc(16);
-    printf(x,23);
+    printf("%dab\n",23);
     return 0;
-    
+
 }
 
 #pragma GCC pop_options
