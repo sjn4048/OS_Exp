af43081f6e41972adb1a94b2ee05621c1b8ae78a
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 06:17:35 +0800

    s

--- kernel/pc/pc.c
+++ kernel/pc/pc.c
@@ -643,7 +643,7 @@ int exec_from_file(char* filename) {
     unsigned int i = 0;
     unsigned int j = 0;
     // unsigned int ENTRY = (unsigned int)kmalloc(4096);
-    unsigned int ENTRY = 0x86000000;
+    unsigned int ENTRY = 0x87000000;
     for (j = 0; j < n; j++) {
         fs_read(&file, buffer, CACHE_BLOCK_SIZE);
         kernel_memcpy((void*)(ENTRY + j * CACHE_BLOCK_SIZE), buffer, CACHE_BLOCK_SIZE);
