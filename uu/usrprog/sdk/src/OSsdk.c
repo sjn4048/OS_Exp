d6fbecc1a08fff4f4bf4ca14f4e6e91cc32f7eb4
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 03:30:18 +0800

    s

--- uu/usrprog/sdk/src/OSsdk.c
+++ uu/usrprog/sdk/src/OSsdk.c
@@ -6,18 +6,19 @@ unsigned int TRANSFORM_ADDR;
 
 
 
-void sdk_init(unsigned int argc, void *args, unsigned int entry_point){
+unsigned int sdk_init(unsigned int argc, void *args, unsigned int entry_point){
     ENTRY_ADDR = entry_point;
     TRANSFORM_ADDR = 0x80000900;
     printf = (int (*)(const char* format, ...))(0x8000282c);
-    main(0,0);
+    unsigned int ret = main(0,0);
+    return ret;
     // TRANSFORM((unsigned int)main,0,0);
 }
 
-unsigned long get_entry_filesize(u8 *entry) {
-    return get_u32(entry + 28);
-}
+// unsigned long get_entry_filesize(u8 *entry) {
+//     return get_u32(entry + 28);
+// }
 
-unsigned long get_u32(u8 *ch) {
-    return (*ch) + ((*(ch + 1)) << 8) + ((*(ch + 2)) << 16) + ((*(ch + 3)) << 24);
-}
+// unsigned long get_u32(u8 *ch) {
+//     return (*ch) + ((*(ch + 1)) << 8) + ((*(ch + 2)) << 16) + ((*(ch + 3)) << 24);
+// }
