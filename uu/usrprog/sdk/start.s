83cc50eaf6f20c95b0d544b079706ec164721090
AlanShaw-GitHub <alanshawzju@gmail.com>
2019-01-09 17:51:10 +0800

    ss

--- uu/usrprog/sdk/start.s
+++ uu/usrprog/sdk/start.s
@@ -1,14 +1,23 @@
-.globl entry
-.set noreorder
-.set noat
-.align 2
+# .globl entry
+# .set noreorder
+# .set noat
+# .align 2
 
-.org 0x0
-entry:
-	# program's address transformation
-	la $t0, sdk_init
-	add $t0, $t0, $a2
-	# jump to main entry and not set $ra
-	# so when main func returns , it directly returns back to OS 
-	j $t0
-	nop
+# .org 0x0
+# entry:
+# 	# program's address transformation
+# 	la $t0, sdk_init
+# 	add $t0, $t0, $a2
+# 	# jump to main entry and not set $ra
+# 	# so when main func returns , it directly returns back to OS 
+# 	j $t0
+# 	nop
+
+.globl seg_start
+seg_start:
+li $t0, 0x12345678
+li $t1, 0xbfc09008
+sw $t0, 0($t1)
+nop
+jr $ra
+nop
