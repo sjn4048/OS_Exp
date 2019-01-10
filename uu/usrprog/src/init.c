6eccf310432336c32fe1a5db03d191f144d7ef46
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 22:25:22 +0800

    s

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -9,10 +9,10 @@
 unsigned int main(unsigned int argc, void *args) {
     // unsigned int ptr = malloc(16);
     // printf("malloc ptr : %x\n",ptr);
+    printf("malloc ptr\n");
     // unsigned int ret =  myvi((char *)args);
     // unsigned int ret =  myvi("test.txt");
     unsigned int ret = ls("/");
-    printf("malloc ptr\n");
 
     // int i = 23;
     return 10;
