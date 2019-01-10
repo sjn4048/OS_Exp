7509927f669923207ed5300b8885fd562b2d7dc9
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 18:09:52 +0800

    s

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -7,7 +7,7 @@
 
 unsigned int main(unsigned int argc, void *args) {
     printf("%dab\n",23);
-    unsigned int ptr = malloc(4096);
+    // unsigned int ptr = malloc(4096);
     // unsigned int ret =  myvi((char *)args);
     // unsigned int ret =  myvi("test.txt");
     // int i = 23;
