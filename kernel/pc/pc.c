022bb3b033a5302f1cf49d868c6cc5f96a629d6b
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 17:04:54 +0800

    s

--- kernel/pc/pc.c
+++ kernel/pc/pc.c
@@ -650,8 +650,8 @@ int exec_from_file(char* filename) {
     unsigned int n = size / CACHE_BLOCK_SIZE + 1;
     unsigned int i = 0;
     unsigned int j = 0;
-// print_proc();
-// kernel_getchar();
+print_proc();
+kernel_getchar();
     unsigned int ENTRY = (unsigned int)kmalloc(8192);
     kernel_printf("size  %x\n",size);
     kernel_printf("ENTRY  %x\n",ENTRY);
@@ -663,9 +663,9 @@ int exec_from_file(char* filename) {
     kernel_printf("ENTRY  %x\n",(unsigned int)kmalloc(4096));
     kernel_printf("ENTRY  %x\n",(unsigned int)kmalloc(4096));
     kernel_printf("ENTRY  %x\n",(unsigned int)kmalloc(4096));
-ENTRY = 0x87000000; 
-// print_proc();
-// kernel_getchar();
+// ENTRY = 0x87000000; 
+print_proc();
+kernel_getchar();
 
     for (j = 0; j < n; j++) {
         fs_read(&file, buffer, CACHE_BLOCK_SIZE);
