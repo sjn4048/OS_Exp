8991b3d15fb4205056bd635666b445ec43e3b6bf
AlanShaw-GitHub <alanshawzju@gmail.com>
2019-01-10 19:30:46 +0800

    s

--- kernel/pc/pc.c
+++ kernel/pc/pc.c
@@ -650,17 +650,11 @@ int exec_from_file(char* filename) {
     unsigned int n = size / CACHE_BLOCK_SIZE + 1;
     unsigned int i = 0;
     unsigned int j = 0;
-    
-
-print_proc();
-// kernel_getchar();
-    unsigned int ENTRY = (unsigned int)kmalloc(4096 * 2);
-    kernel_printf("size  %x\n",size);
+    unsigned int ENTRY = (unsigned int)kmalloc(4096);
     kernel_printf("ENTRY  %x\n",ENTRY);
  
 print_proc();
-// kernel_getchar();
-
+kernel_getchar();
     for (j = 0; j < n; j++) {
         fs_read(&file, buffer, CACHE_BLOCK_SIZE);
         kernel_memcpy((void*)(ENTRY + j * CACHE_BLOCK_SIZE), buffer, CACHE_BLOCK_SIZE);
@@ -668,7 +662,6 @@ print_proc();
     }
     int (*f)(unsigned int argc, void *args, unsigned int addr) = (int (*)(unsigned int argc, void *args, unsigned int addr))(ENTRY);
     unsigned int ass = 0;
-
 asm volatile("move %0, $sp\n\t" : "=r"(ass));
 kernel_printf("before  %x\n",ass);
 // flag = 1;
@@ -679,7 +672,7 @@ asm volatile("move %0, $sp\n\t" : "=r"(ass));
 kernel_printf("after  %x\n",ass);
 
     // disable_interrupts();
-    return ret;
+    return 0;
 
 }
 
