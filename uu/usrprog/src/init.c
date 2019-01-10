06f442c53689475d8ae487f3782b75c4f5a071c3
Alan-Shaw <alanshawzju@gmail.com>
2019-01-11 02:35:51 +0800

    s

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -7,18 +7,21 @@
 
 
 unsigned int main(unsigned int argc, char params[][10]) {
-    // unsigned int ptr = malloc(16);
-    // printf("malloc ptr : %x\n",ptr);
+    printf("we are in the user mode!\n");
+    printf("begin to malloc a space!\n");
+    unsigned int ptr = malloc(16);
+    printf("malloc ptr : %x\n",ptr);
+    printf("ending!\n");
 
     // unsigned int ret =  myvi((char *)args);
     // unsigned int ret =  myvi("test.txt");
     
     // unsigned int ret = ls((char *)args);
-    unsigned int ret;
-    if (argc > 1)
-        ret = ls(params[1]);
-    else
-        ret = ls("/");
+    // unsigned int ret;
+    // if (argc > 1)
+    //     ret = ls(params[1]);
+    // else
+    //     ret = ls("/");
 
     return 0;
 }
