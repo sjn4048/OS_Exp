7bd6746d6b71c2e741b35b2c2ae66d02d7a1cb34
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 19:55:49 +0800

    s

--- kernel/syscall/syscall.c
+++ kernel/syscall/syscall.c
@@ -35,6 +35,7 @@ void __syscall(unsigned int status, unsigned int cause, context* pt_context) {
     code = pt_context->t0;
     pt_context->epc += 4;
     kernel_printf("syscall!!!!!! %d\n",code);
+    kernel_printf("syscall!!!!!! %x\n",syscalls[code]);
     if (syscalls[code]) {
         pt_context->v0 = syscalls[code](status, cause, pt_context);
     }
