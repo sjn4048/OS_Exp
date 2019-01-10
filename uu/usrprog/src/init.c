c33f668a75f96eb889728d015575ed8c241a7c4c
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 22:17:11 +0800

    s

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -1,6 +1,6 @@
 #include <init.h>
 // #include "myvi.h"
-#include "ls.h"
+// #include "ls.h"
 
 #pragma GCC push_options
 #pragma GCC optimize("O0")
@@ -12,7 +12,7 @@ unsigned int main(unsigned int argc, void *args) {
     // unsigned int ret =  myvi((char *)args);
     // unsigned int ret =  myvi("test.txt");
     // unsigned int ret = ls("/");
-    printf("malloc ptr\n");
+    // printf("malloc ptr\n");
 
     // int i = 23;
     return 10;
