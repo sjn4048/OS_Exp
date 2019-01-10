18d6ec1df4c2a156f9ccde07c0b197089f49eebe
Alan-Shaw <alanshawzju@gmail.com>
2019-01-10 18:16:58 +0800

    s

--- kernel/syscall/syscalls.c
+++ kernel/syscall/syscalls.c
@@ -20,7 +20,7 @@ unsigned int kernel_printf_syscall(unsigned int status, unsigned int cause, cont
 unsigned int kmalloc_syscall(unsigned int status, unsigned int cause, context* pt_context){
     const unsigned int size = (unsigned int)pt_context->a0;
     void * ptr = kmalloc(size);
-    // kernel_printf("aaa%x\n",(unsigned int)ptr);
+    kernel_printf("aaa%x\n",(unsigned int)ptr);
     return (unsigned int)ptr;
 }
 
